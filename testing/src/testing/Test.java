c4f019a701b1dac9c536bd15bdc1cd935ac00af7
indu-rani <indusandeepb@gmail.com>
2022-11-10 18:44:19 +0530

    first commit

---
+++ testing/src/testing/Test.java
@@ -0,0 +1,15 @@
+package testing;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Test1 t = new Test1();
+		t.test();
+	}
+
+}
+class Test1{
+	public void test(){
+		System.out.print("hello");
+	}
+}
---
+++ testing/src/testing/second.java
@@ -0,0 +1,9 @@
+package testing;
+
+public class second {
+
+	public static void main(String[] s){
+		Test1 t = new Test1();
+		t.test();
+	}
+}
